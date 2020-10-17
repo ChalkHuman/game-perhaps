88c61443d9d86d4dacae6e773c5927d38da9eedb
ChalkHuman <isacisboss718@gmail.com>
2020-10-17 16:44:03 -0500

    player controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private void Update ()
+    {
+        if (Input.GetKey (KeyCode.A))
+        {
+            rb.velocity = new Vector2 (-5, rb.velocity.y);
+        }
+
+        if (Input.GetKey (KeyCode.D))
+        {
+            rb.velocity = new Vector2 (5, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            rb.velocity = new Vector2 (rb.velocity.x, 10);
+        }
+    }
+}
