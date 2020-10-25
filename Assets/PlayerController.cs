f1015036c9c85d6589cf86b793b819331affefbb
ChalkHuman <isacisboss718@gmail.com>
2020-10-25 17:40:17 -0500

    better input

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+	private Animator anim;
+
+    private void Start ()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator> ();
+    }
 
     private void Update ()
     {
-        if (Input.GetKey (KeyCode.A))
+        float hDirection = Input.GetAxis ("Horizontal");
+
+        if (hDirection < 0)
         {
             rb.velocity = new Vector2 (-5, rb.velocity.y);
 			transform.localScale = new Vector2 (-1, 1);
+			anim.SetBool ("running", true);
         }
 
-        if (Input.GetKey (KeyCode.D))
+        else if (hDirection > 0)
         {
             rb.velocity = new Vector2 (5, rb.velocity.y);
 			transform.localScale = new Vector2 (1, 1);
+			anim.SetBool ("running", true);
         }
+
+		else {
+			anim.SetBool ("running", false);
+		}
 
         if (Input.GetKeyDown (KeyCode.Space))
         {
