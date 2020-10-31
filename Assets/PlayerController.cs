c752b7601068392c58ce684dd7b8f51977e1dfe4
ChalkHuman <isacisboss718@gmail.com>
2020-10-31 13:58:10 -0500

    better state machine

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,13 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
 	private Animator anim;
-    private enum State {idle, running, jumping}
+    private enum State {idle, running, jumping, falling}
     private State state = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
 
     private void Start ()
     {
         rb = GetComponent<Rigidbody2D> ();
         anim = GetComponent<Animator> ();
+        coll = GetComponent<Collider2D> ();
     }
 
     private void Update ()
@@ -31,12 +34,12 @@ public class PlayerController : MonoBehaviour
 			transform.localScale = new Vector2 (1, 1);
         }
 
-        if (Input.GetKeyDown (KeyCode.Space))
+        if (Input.GetButtonDown ("Jump") && coll.IsTouchingLayers (ground))
         {
             rb.velocity = new Vector2 (rb.velocity.x, 10);
             state = State.jumping;
         }
-        
+
         else {
             state = State.idle;
         }
@@ -49,15 +52,23 @@ public class PlayerController : MonoBehaviour
     {
         if (state == State.jumping)
         {
-            //Jumping
-            Debug.Log ("jumping");
+            if (rb.velocity.y < .1f)
+            {
+                state = State.falling;
+            }
+            
+            else if (state == State.falling) {
+                if (coll.IsTouchingLayers (ground))
+                {
+                    state = State.idle;
+                }
+            }
         }
 
         else if (Mathf.Abs (rb.velocity.x) > 2f)
         {
             //Moving
             state = State.running;
-            Debug.Log (rb.velocity.x);
         }
 
         else
